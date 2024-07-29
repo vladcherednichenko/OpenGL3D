dfa85964d8b9b5af853afa1cb9df549a49a989dd
vlad <fumedancer@gmail.com>
2024-07-29 19:03:45 +0300

    light and textures

--- NormalMapping/graphics3dx/src/main/assets/shaders/fragment_shader.glsl
+++ NormalMapping/graphics3dx/src/main/assets/shaders/fragment_shader.glsl
@@ -1,12 +1,21 @@
 precision mediump float;
 uniform sampler2D texture;
 varying vec2 textureCord;
-varying vec3 lighting;
+varying vec3 diffuseLighting;
+varying vec3 ambientLighting;
+varying vec3 specularLighting;
 void main()
 {
 
     vec4 textureColor = texture2D(texture, textureCord);
-    gl_FragColor = textureColor * vec4(lighting, 1.0);
+
+    vec4 finalColor = vec4(0.0);
+    finalColor += vec4(diffuseLighting, 1.0) * textureColor;
+    finalColor += vec4(ambientLighting, 1.0) * textureColor;
+    finalColor += vec4(specularLighting, 1.0) * textureColor;
+    finalColor = clamp(finalColor, 0.0, 1.0);
+
+    gl_FragColor = finalColor;
 
 }
 
--- NormalMapping/graphics3dx/src/main/assets/shaders/vertex_shader.glsl
+++ NormalMapping/graphics3dx/src/main/assets/shaders/vertex_shader.glsl
@@ -5,32 +5,44 @@ uniform mat4 projection;
 uniform mat4 modelView;
 
 varying vec2 textureCord;
-varying vec3 lighting;
+varying vec3 diffuseLighting;
+varying vec3 ambientLighting;
+varying vec3 specularLighting;
+
 
 void main()
 {
 
+    /* [Setup scene vectors.] */
     vec3 transformedVertexNormal = normalize((modelView * vec4(vertexNormal, 0.0)).xyz);
     vec3 inverseLightDirection = normalize(vec3(0.0, 1.0, 1.0));
     vec3 fragColour = vec3(0.0);
+    /* [Setup scene vectors.] */
 
-    vec3 diffuseLightIntensity = vec3(0.5, 0.5, 0.5);
+    /* [Calculate the diffuse component.] */
+    float diffuse = 0.5;
+    vec3 diffuseLightIntensity = vec3(diffuse);
     float normalDotLight = max(0.0, dot(transformedVertexNormal, inverseLightDirection));
-    fragColour += normalDotLight * diffuseLightIntensity;
+    diffuseLighting = normalDotLight * diffuseLightIntensity;
+    /* [Calculate the diffuse component.] */
 
-    float ambientLight = 0.1;
-    vec3 ambientLightIntensity = vec3(ambientLight, ambientLight, ambientLight);
-    fragColour += ambientLightIntensity;
+    /* [Calculate the ambient component.] */
+    float ambient = 0.1;
+    vec3 ambientLightIntensity = vec3(ambient);
+    ambientLighting = ambientLightIntensity;
+    /* [Calculate the ambient component.] */
 
+    /* [Calculate the specular component.] */
     vec3 inverseEyeDirection = normalize(vec3(0.0, 0.0, 1.0));
-    vec3 specularLightIntensity = vec3(1.0, 1.0, 1.0);
+    float specular = 1.0;
+    vec3 specularLightIntensity = vec3(specular);
+    vec3 vertexSpecularReflectionConstant = vec3(1.0, 1.0, 1.0);
     float shininess = 2.0;
     vec3 lightReflectionDirection = reflect(vec3(0) - inverseLightDirection, transformedVertexNormal);
     float normalDotReflection = max(0.0, dot(inverseEyeDirection, lightReflectionDirection));
-    fragColour += pow(normalDotReflection, shininess) * specularLightIntensity;
+    specularLighting = pow(normalDotReflection, shininess) * vertexSpecularReflectionConstant * specularLightIntensity;
+    /* [Calculate the specular component.] */
 
-
-    lighting = clamp(fragColour, 0.0, 1.0);
 
     gl_Position = projection * modelView * vertexPosition;
     textureCord = vertexTextureCord;
